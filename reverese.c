7b6703a08f780a4bd266c7039ee81d0cab7778be
Aravind Reddy <aravindreddy.dondapati@votarytech.com>
2026-02-11 18:24:29 +0530

    added reverse to calculator_project

---
+++ reverese.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+int main()
+{
+	int num, rem, rev = 0;
+	printf("enter the num: ");
+	scanf("%d" &num);
+
+	while(num != 0)
+	{
+		rem = num % 10;
+		rev = rev + (rem*10);
+		num /= 10;
+	}
+	printf("%d", num);
+	return 0; 
+}
